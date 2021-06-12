c58b372cf0bbbcbcc35cac44c4869dca627c306c
Rafael Rocha de Azevedo <rrochadeazevedo@gmail.com>
2021-06-12 18:29:23 -0300

    feat: configures github OAuth, and installs faunaDB

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,12 +1,14 @@
 import { FaGithub } from 'react-icons/fa';
-import { FiX } from 'react-icons/fi'
+import { FiX } from 'react-icons/fi';
+
+import { signIn, useSession } from 'next-auth/client';
 
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-    const isUserLoggedIn = true;
+    const [session] = useSession();
 
-    return isUserLoggedIn ? (
+    return session ? (
         <button type="button"
             className={styles.signInButton}>
             <FaGithub color="#04d361" />
@@ -17,7 +19,9 @@ export function SignInButton() {
 
         </button>
     ) : (
-        <button type="button"
+        <button
+            onClick={() => signIn('github')}
+            type="button"
             className={styles.signInButton}>
             <FaGithub color="#eba417" />
             Sign in with Github
